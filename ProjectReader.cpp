ca069147deba57b55a22f076c288276a5481d1ac
nicklim011213 <nicklim011213@gmail.com>
2024-10-14 22:38:17 -0400

    Continue work on the project reader.
    Focus on Item class representing a single Item. Include methods for inserting vertexes. Next is inserting triangles using three different indexes.

--- ProjectReader.cpp
+++ ProjectReader.cpp
@@ -21,7 +21,6 @@ public:
 			if (VersionString.find("0.1A") != VersionString.npos)
 			{
 				std::cout << "Version 0.1 Alpha\n";
-				SendToRender(ItemParsing(Filehandle));
 			}
 		}
 
@@ -37,3 +36,39 @@ public:
 private:
 	std::string FilePath;
 };
+
+	void Item::InsertVertex(const Space3D& Vertex, const unsigned int position)
+	{
+		if (position > VertexList.size())
+		{
+			std::cerr << "Inserted index is at invalid position ignoring...\n";
+			return;
+		}
+
+		VertexList.insert(VertexList.begin() + position, Vertex);
+
+		// Correct Vertex Index Numbers
+		for (int i = 0; i != VertexIndex.size(); ++i)
+		{
+			if (VertexIndex[i] >= position)
+			VertexIndex[i] += 1;
+		}
+	}
+
+	void Item::InsertVertex(const Space3D& Vertex, const bool Start)
+	{
+		if (!Start)
+		{
+			VertexList.push_back(Vertex);
+		}
+		else
+		{
+			VertexList.push_front(Vertex);
+
+			// Correct Vertex Index Numbers
+			for (int i = 0; i != VertexIndex.size(); ++i)
+			{
+				VertexIndex[i] += 1;
+			}
+		}
+	}
--- ProjectReader.h
+++ ProjectReader.h
@@ -4,6 +4,7 @@
 	#include <fstream>
 	#include <string>
 	#include <unordered_map>
+	#include <deque>
 		class ProjectHandler
 		{
 		public:
@@ -16,9 +17,36 @@
 			
 		};
 
+		struct Space3D
+		{
+			int X;
+			int Y;
+			int Z;
+		};
+
 		class Item
 		{
-			std::unordered_map<
+			private:
+				// A unique ID to refrence the Item by
+				char* ItemID[512];
+
+				// Holds a list of vertexs used in the model of the item
+				std::deque<Space3D> VertexList;
+
+				// Holds a deque of indexes of vertexs in Vertexlist that make up the triangles for renderings
+				std::deque<int> VertexIndex;
+
+			public:
+				// Inserts a Vertex and shifts VertexIndexs +1 
+				void InsertVertex(const Space3D&, const unsigned int position);
+
+				// Overload that inserts at start or end if false
+				void InsertVertex(const Space3D&, const bool Start);
+
+				//TODO: Create a function that validates if VertexIndex contains only valid indexes
+
+				// Inserts a Triangle 
+				void InsertFace(int Index1, int Index2, int Index3);
 		};
 
 		class ItemCollection
